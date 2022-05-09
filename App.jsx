d938044436cf57a2665e0b812696b35196d5f518
Lidianne Barbosa <lidiannerb@gmail.com>
2022-05-09 18:23:07 -0300

    Updates

---
+++ App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import "./App.css";
+
+
+const App = () => {
+  const [ tasks, setTasks ] = useState([
+    {
+      id: "1",
+      title: "Estudar programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      completed: true,
+    }, 
+  ]);
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId) return {...task, completed: !task.completed}
+
+      return task;
+    });
+
+    setTasks(newTasks)
+  };
+  
+  /* a const abaixo vai adicionar as tasks no useState acima, quando passarmos ele para o AddTask lá embaixo*/
+
+  const handleTaskAddition = (taskTitle) => {    
+    const newTasks = [ 
+      ...tasks, 
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+    setTasks(newTasks);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} 
+        handleTaskClick={handleTaskClick} 
+        handleTaskDeletion={handleTaskDeletion} />  
+      </div> 
+    </>
+  );    
+};
+
+export default App;
